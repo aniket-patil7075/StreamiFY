e04b4a3a3a0548329e32fe41aec003c4b6b7bb88
aniket-patil7075 <akp7075@gmail.com>
2024-09-10 18:12:25 +0530

    Navbar done

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
->>>>>>> fea5f2c5b7f930138d342a3e97f4fbc968c24e16
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
